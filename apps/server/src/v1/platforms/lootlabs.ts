4490c423bbc454a54109d84cd5477c1a408eadf4
itsnoctural <85783226+itsnoctural@users.noreply.github.com>
2024-09-04 20:08:46 +0300

    fix(api): lootlabs redirect generated without tk

--- apps/server/src/v1/platforms/lootlabs.ts
+++ apps/server/src/v1/platforms/lootlabs.ts
@@ -1,9 +1,13 @@
 import { error } from "elysia";
 
 async function encrypt(dest: string, apiKey: string) {
-  return await fetch(
-    `https://be.lootlabs.gg/api/lootlabs/url_encryptor?destination_url=${dest}&api_token=${apiKey}`,
-  ).then(async (response) => {
+  return await fetch("https://be.lootlabs.gg/api/lootlabs/url_encryptor", {
+    method: "POST",
+    headers: {
+      Authorization: apiKey,
+    },
+    body: JSON.stringify({ destination_url: dest }),
+  }).then(async (response) => {
     if (!response.ok) throw error(400, "Bad Request. Invalid API Key");
 
     const data = await response.json();
