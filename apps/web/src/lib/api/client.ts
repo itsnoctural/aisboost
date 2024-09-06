06cbe146f3510b4d8944077f8305671104cd37bc
itsnoctural <85783226+itsnoctural@users.noreply.github.com>
2024-09-06 21:03:21 +0300

    fix(web): workaround for eden types

--- apps/web/src/lib/api/client.ts
+++ apps/web/src/lib/api/client.ts
@@ -1,6 +1,7 @@
 import type { App } from "@aisboost/server";
 import { treaty } from "@elysiajs/eden";
 
+// @ts-ignore
 export const api = treaty<App>(process.env.NEXT_PUBLIC_API, {
   fetch: { credentials: "include" },
 });
--- apps/web/src/lib/api/server.ts
+++ apps/web/src/lib/api/server.ts
@@ -2,6 +2,7 @@ import type { App } from "@aisboost/server";
 import { treaty } from "@elysiajs/eden";
 import { headers } from "next/headers";
 
+// @ts-ignore
 export const api = treaty<App>(process.env.NEXT_PUBLIC_API, {
   headers: () => {
     return { Cookie: headers().get("Cookie") || "" };
