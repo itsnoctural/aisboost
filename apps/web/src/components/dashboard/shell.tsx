26ed89fed616ee09ce20540257a953bed4c567ea
itsnoctural <85783226+itsnoctural@users.noreply.github.com>
2024-09-08 11:00:59 +0300

    style(web): decrease shell padding on mobile

--- apps/web/src/components/dashboard/shell.tsx
+++ apps/web/src/components/dashboard/shell.tsx
@@ -1,7 +1,7 @@
 export function Shell({ children }: { children: React.ReactNode }) {
   return (
     <main className="mx-auto flex w-full max-w-screen-xl min-h-screen my-4">
-      <div className="flex flex-col gap-y-8 w-full rounded-lg border border-border p-8 bg-background/85">
+      <div className="flex flex-col gap-y-8 w-full rounded-lg border border-border p-4 md:p-8 bg-background/85">
         {children}
       </div>
     </main>
