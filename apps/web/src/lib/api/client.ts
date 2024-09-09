94c04fe1c4f99125552f808850a499690e1fe881
itsnoctural <85783226+itsnoctural@users.noreply.github.com>
2024-09-09 17:09:27 +0300

    Revert "fix(web): workaround for eden types"
    This reverts commit 06cbe146f3510b4d8944077f8305671104cd37bc.

--- apps/web/src/lib/api/client.ts
+++ apps/web/src/lib/api/client.ts
@@ -1,7 +1,6 @@
 import type { App } from "@aisboost/server";
 import { treaty } from "@elysiajs/eden";
 
-// @ts-ignore
 export const api = treaty<App>(process.env.NEXT_PUBLIC_API, {
   fetch: { credentials: "include" },
 });
--- apps/web/src/lib/api/server.ts
+++ apps/web/src/lib/api/server.ts
@@ -2,7 +2,6 @@ import type { App } from "@aisboost/server";
 import { treaty } from "@elysiajs/eden";
 import { headers } from "next/headers";
 
-// @ts-ignore
 export const api = treaty<App>(process.env.NEXT_PUBLIC_API, {
   headers: () => {
     return { Cookie: headers().get("Cookie") || "" };
