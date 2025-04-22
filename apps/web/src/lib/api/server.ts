d99211bb7b6cf9de45133e34781d839028240144
itsnoctural <85783226+itsnoctural@users.noreply.github.com>
2025-04-22 18:00:37 +0000

    chore(api): update fly.io conf

--- apps/web/src/lib/api/server.ts
+++ apps/web/src/lib/api/server.ts
@@ -1,12 +1,12 @@
 import type { App } from "@aisboost/server";
 import { treaty } from "@elysiajs/eden";
-import { headers, type UnsafeUnwrappedHeaders } from "next/headers";
+import { headers } from "next/headers";
 
 export const api = treaty<App>(process.env.NEXT_PUBLIC_API, {
-  headers: () => {
+  headers: async () => {
     return {
       Cookie:
-        (headers() as unknown as UnsafeUnwrappedHeaders).get("Cookie") || "",
+        (await headers()).get("Cookie") || "",
     };
   },
 });
